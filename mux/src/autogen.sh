fe7afc1386d811ef1532514c289ca6a35efd918d
Emily Backes <lucca@accela.net>
2015-01-01 14:08:29 -0800

    Fix thinko in autogen.sh to call autoreconf
    Realistically, we could use -fiv and then sed the .ac files to fixup the
    libtool paths for m4 and config to ../m4 and ../config and then
    re-autoreconf -iv afterward.
    
    That's a mess.  "There has to be a better way."

--- mux/src/autogen.sh
+++ mux/src/autogen.sh
@@ -2,7 +2,7 @@
 
 # do not use -f (--force); the ltdl install has customizations to use
 # local m4 and config aux dirs.
-autoconf -iv
+autoreconf -iv
 
 # similarly, clean up the blindly copied subdirs
 rm -rf libltdl/config libltdl/m4
