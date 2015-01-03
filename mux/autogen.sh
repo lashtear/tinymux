023ec37441a649a16c0871250aad23827971b36d
Emily Backes <lucca@accela.net>
2015-01-03 07:46:43 -0800

    Robustify autogen

--- mux/autogen.sh
+++ mux/autogen.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
-
-rm -rf libltdl && libtoolize --nonrecursive --copy --quiet
+set -e
+echo Removing old build infrastructure
+rm -rf aclocal.m4 autom4te.cache config libltdl m4
+echo LIBTOOLIZE
+libtoolize --nonrecursive --copy --quiet
+echo ACLOCAL
 aclocal -I m4
+echo AUTOHEADER
 autoheader
-automake
+echo AUTOMAKE
+automake --add-missing --copy
+echo AUTOCONF
 autoconf
